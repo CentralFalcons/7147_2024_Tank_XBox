2ddf102b0245518f8952925a76d8b99bc394d9c8
Central Academy Students <centralfalconsrobotics@gee-edu.com>
2024-03-08 08:35:43 -0500

    belleville event

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,14 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+// import com.revrobotics.REVLibError;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -28,13 +33,17 @@ public class Robot extends TimedRobot {
   private MotorController m_shooterFront = new WPI_VictorSPX(5);
   private MotorController m_shooterRear = new WPI_VictorSPX(6);
 
+  private CANSparkMax m_intake = new CANSparkMax(7, MotorType.kBrushed);
+
   private DifferentialDrive m_robotDrive = new DifferentialDrive(m_frontLeft::set, m_frontRight::set);
 
   private XboxController driver_controller = new XboxController(0);
   private XboxController shooter_controller = new XboxController(1);
 
-  private static double INPUT_SPEED = .25;
-  private static double OUTPUT_SPEED = .25;
+  private static double INPUT_SPEED = .50;
+  private static double OUTPUT_SPEED_FRONT = 1;
+  private static double OUTPUT_SPEED_REAR = 1;
+  private static double INTAKE_SPEED = .25;
 
   public Robot() {
     SendableRegistry.addChild(m_robotDrive, m_frontLeft);
@@ -52,6 +61,14 @@ public class Robot extends TimedRobot {
     m_rearRight.set(TalonSRXControlMode.Follower,3);
     m_frontRight.setInverted(true);
     m_rearRight.setInverted(true);
+    m_frontRight.setNeutralMode(NeutralMode.Brake);
+    m_frontLeft.setNeutralMode(NeutralMode.Brake);
+    m_rearRight.setNeutralMode(NeutralMode.Brake);
+    m_rearLeft.setNeutralMode(NeutralMode.Brake);
+    m_frontRight.configNeutralDeadband(0);
+    m_frontLeft.configNeutralDeadband(0);
+    m_rearRight.configNeutralDeadband(0);
+    m_rearLeft.configNeutralDeadband(0);
   }
 
   @Override
@@ -59,17 +76,38 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-driver_controller.getRightY(), -driver_controller.getLeftX()*.5);
+if(driver_controller.getRightBumper()){
+   m_robotDrive.tankDrive(-driver_controller.getLeftY()*0.75, -driver_controller.getRightY()*0.75);
+} else {
+   m_robotDrive.tankDrive(-driver_controller.getLeftY(), -driver_controller.getRightY());
+}
 
+
+    // Shooter Front
     if(shooter_controller.getYButton()){
-      m_shooterFront.set(OUTPUT_SPEED);
-      m_shooterRear.set(OUTPUT_SPEED);
-    } else if(shooter_controller.getAButton()){
-      m_shooterFront.set(INPUT_SPEED);
-      m_shooterRear.set(INPUT_SPEED);
-    } else {
+      m_shooterFront.set(OUTPUT_SPEED_FRONT);
+    } else if(shooter_controller.getAButton())
+      m_shooterFront.set(-INPUT_SPEED);
+    else {
       m_shooterFront.set(0);
+    }
+
+    // Shooter Rear
+    if(shooter_controller.getRightBumper()){
+      m_shooterRear.set(OUTPUT_SPEED_REAR);
+    } else if(shooter_controller.getAButton())
+      m_shooterFront.set(-INPUT_SPEED); 
+    else {
       m_shooterRear.set(0);
+    }
+    
+    //intake
+    if (shooter_controller.getBButton()){
+      m_intake.set(INTAKE_SPEED);
+    } else if (shooter_controller.getXButton()) {
+      m_intake.set(-INTAKE_SPEED);
+    } else {
+      m_intake.set(0);
     }
   }
 }
